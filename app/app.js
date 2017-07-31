d86da9fbe86eba133a023a8d8e8db14ef46c29a1
GeekInterrupted <GeekInterrupted@users.noreply.github.com>
2017-07-31 14:49:51 -0500

    updated file

---
+++ app/app.js
@@ -0,0 +1,15 @@
+//Main React Dependencies
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./components/Main";
+
+//grab routes
+
+// import RouteMain from "./config/routes";
+
+//renders the contents according to the route page
+// ReactDOM.render(<RouteMain />, document.getElementById("app"));
+
+ReactDOM.render(<Main />, document.getElementById("app"));
+
+
